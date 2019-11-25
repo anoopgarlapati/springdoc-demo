59324eee28aa3373c087ee92e9defd7fcc2d4888
Anoop Garlapati <gsrivna@vmware.com>
2019-11-25 22:54:44 +0530

    Initialize project
    - Initialized speing boot project
    with gradle configured.

---
+++ src/main/java/com/dio/demo/springdoc/SpringdocDemoApplication.java
@@ -0,0 +1,13 @@
+package com.dio.demo.springdoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringdocDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringdocDemoApplication.class, args);
+	}
+
+}
